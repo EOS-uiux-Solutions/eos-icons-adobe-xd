3027b1a377aa2219569d88c9c4782ee0c8f99234
Abhirup-99 <abhiruppalmethodist@gmail.com>
2021-08-06 23:54:54 +0530

    add icon reset functionality and wrap functions with useCallback.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import ReactDOMServer from "react-dom/server";
 import * as EOSIcons from "eos-icons-react";
 // eslint-disable-next-line import/no-unresolved
@@ -17,14 +17,14 @@ const App = () => {
   );
   const [alert, updateAlert] = useState(false);
   const [iconsContainer, updateIcons] = useState(null);
-  const copyToClipboard = (svg) => {
+  const copyToClipboard = useCallback((svg) => {
     copyText(ReactDOMServer.renderToStaticMarkup(svg));
     updateAlert(true);
     setTimeout(() => {
       updateAlert(false);
     }, 1000);
-  };
-  const searchIconsByName = (name, theme, option) => {
+  }, []);
+  const searchIconsByName = useCallback((name, theme, option) => {
     const icons = [];
     EOSIconsList[option].forEach((icon) => {
       const isFilledAvailable = "hasOutlined" in icon && icon.hasOutlined;
@@ -51,41 +51,43 @@ const App = () => {
         copyToClipboard={copyToClipboard}
       />
     );
-  };
-  useEffect(() => {
-    const createIcons = (option) => {
-      const icons = [];
-      const limit =
-        EOSIconsList[option].length < 10 ? EOSIconsList[option].length : 10;
-      // eslint-disable-next-line no-plusplus
-      for (let i = 0; i < limit; i++) {
-        const icon = EOSIconsList[option][i];
-        const name = EOSIcons[`Eos_${icon.name}_Filled`.toUpperCase()]
-          ? `Eos_${icon.name}_Filled`.toUpperCase()
-          : `Eos_${icon.name}_Outlined`.toUpperCase();
-        const EOSReactIcon = EOSIcons[name];
-        if (EOSReactIcon !== undefined) {
-          icons.push({ EOSReactIcon, name });
-        }
+  }, []);
+  const createIcons = useCallback((option) => {
+    const icons = [];
+    const limit =
+      EOSIconsList[option].length < 10 ? EOSIconsList[option].length : 10;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < limit; i++) {
+      const icon = EOSIconsList[option][i];
+      const name = EOSIcons[`Eos_${icon.name}_Filled`.toUpperCase()]
+        ? `Eos_${icon.name}_Filled`.toUpperCase()
+        : `Eos_${icon.name}_Outlined`.toUpperCase();
+      const EOSReactIcon = EOSIcons[name];
+      if (EOSReactIcon !== undefined) {
+        icons.push({ EOSReactIcon, name });
       }
+    }
 
-      return (
-        <IconBox
-          option={option}
-          icons={icons}
-          key={option}
-          copyToClipboard={copyToClipboard}
-        />
-      );
-    };
+    return (
+      <IconBox
+        option={option}
+        icons={icons}
+        key={option}
+        copyToClipboard={copyToClipboard}
+      />
+    );
+  }, []);
+
+  useEffect(() => {
     updateIcons(OptionsList.map((option) => createIcons(option)));
   }, []);
 
-  const clearValue = () => {
+  const clearValue = useCallback(() => {
     inputField.current.value = "";
     setHelperText("Let's start by searching abstract.");
-  };
-  const onSearch = () => {
+    updateIcons(OptionsList.map((option) => createIcons(option)));
+  }, []);
+  const onSearch = useCallback(() => {
     const category = searchCategory.current.value;
     const theme = searchTheme.current.value;
     const name = inputField.current.value;
@@ -98,16 +100,15 @@ const App = () => {
       iconList = searchIconsByName(name, theme, category);
     }
     updateIcons(iconList);
-  };
+  }, []);
 
-  const handleKeyUp = (event) => {
+  const handleKeyUp = useCallback((event) => {
     setHelperText(`We would be searching for ${inputField.current.value}`);
-    console.log(event.key);
     if (event.key === "ArrowRight") {
       event.preventDefault();
       onSearch();
     }
-  };
+  }, []);
 
   return (
     <React.Fragment>
